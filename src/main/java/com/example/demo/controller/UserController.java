c6103a978a28bf96dafc27a278d951876a526f5b
ctrlpdf <ctrlpdf@gmail.com>
2023-05-04 10:17:43 +0900

    Added /signup api

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,47 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+  private final UserService userService;
 
-  @GetMapping("/hello")
-  public ResponseEntity<String> hello() {
-    return ResponseEntity.ok("hello");
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/test-redirect")
+  public void testRedirect(HttpServletResponse response) throws IOException {
+    response.sendRedirect("/api/user");
+  }
+
+  @PostMapping("/signup")
+  public ResponseEntity<UserDto> signup(
+          @Valid @RequestBody UserDto userDto
+  ) {
+    return ResponseEntity.ok(userService.signup(userDto));
+  }
+
+  @GetMapping("/user")
+  @PreAuthorize("hasAnyRole('USER','ADMIN')")
+  public ResponseEntity<UserDto> getMyUserInfo(HttpServletRequest request) {
+    return ResponseEntity.ok(userService.getMyUserWithAuthorities());
+  }
+
+  @GetMapping("/user/{username}")
+  @PreAuthorize("hasAnyRole('ADMIN')")
+  public ResponseEntity<UserDto> getUserInfo(@PathVariable String username) {
+    return ResponseEntity.ok(userService.getUserWithAuthorities(username));
   }
 
 }
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,63 @@
+package com.example.demo.service;
+
+import java.util.Collections;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.Authority;
+import com.example.demo.entity.User;
+import com.example.demo.exception.DuplicateMemberException;
+import com.example.demo.exception.NotFoundMemberException;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.SecurityUtil;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public UserDto signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByEmail(userDto.getEmail()).orElse(null) != null) {
+            throw new DuplicateMemberException("You are already signed up");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .email(userDto.getEmail())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return UserDto.from(userRepository.save(user));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getUserWithAuthorities(String email) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByEmail(email).orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(
+                SecurityUtil.getCurrentUsername()
+                        .flatMap(userRepository::findOneWithAuthoritiesByEmail)
+                        .orElseThrow(() -> new NotFoundMemberException("Member not found"))
+        );
+    }
+
+}
